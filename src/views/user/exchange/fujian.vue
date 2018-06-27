f2c62c10f22c88cbf4e1706f9046ffb2c90c66b9
hjm100 <hjm100@126.com>
2018-06-27 14:02:37 +0800

    优化列表

--- src/views/user/exchange/fujian.vue
+++ src/views/user/exchange/fujian.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="exchangeIndex">
-      <dida-list ref="dida_list" post-module="prizes" post-url="prizeList" @returnData="getPrizeList($event)">
+      <van-list v-model="loading" :finished="finished" @load="onLoad">
         <van-panel class="panel" v-for="(i,index) in prizeList" :key="index">
           <div class="panel_header" slot="header">
             <van-cell value="2018/5/3 12:34:45" label="10000001" title="用户昵">
@@ -9,58 +9,103 @@
           </div>
           <van-card title="九阳榨汁机" desc="收货地址：北京市昌平区回龙观东大街338号腾讯众创空间 A322  陈奕迅  18888888888" currency="" price="邮寄奖品" :thumb="imageURL"/>
           <div class="footer" slot="footer">
-              <van-button style="margin-right:15px;" class="btn gobtn" size="small" @click="showAlert(index)">去发货</van-button>
-              <van-button class="btn sendbtn" size="small" @click="showPopup(index)">未取货</van-button>
+              <van-button class="btn gobtn" size="small" @click="showAlert(index)">去发货</van-button>
+              <van-button class="btn gobtn" size="small" @click="showPopup(index)">未取货</van-button>
           </div>
         </van-panel>
-      </dida-list>
-      <van-dialog title= '嘀嗒比赛' v-model="showDialog" show-cancel-button :before-close="beforeClose">
-          <van-field v-model="express_name" label="快递公司" placeholder="请输入快递公司名称"/>
-          <van-field v-model="express_number" label="快递单号" placeholder="请输入快递单号"/>
-      </van-dialog>
-      <number-word-input ref="number_word_input" @goodsIdAjax="goodsIdAjax($event)"></number-word-input>
+        <!-- <van-panel v-for="prize in prizeList" :key="prize.id" class="panel">
+            <div class="panel_header" slot="header">
+              <van-cell :value="prize.time" :label="prize.matchdec" :title="prize.nickname">
+                <img slot="icon" :src="prize.icon" alt="" srcset="">
+              </van-cell>
+            </div>
+            <van-card :title="prize.prize.name" :desc="prize.receivingDec" :price="prize.prizeType" :thumb="prize.prize.img"/>
+            邮寄物品
+            <div v-if="prize.prize.type == 0" class="footer" slot="footer">
+                <van-button v-if="prize.receiving.status==2" class="btn sendbtn" size="small">已发货</van-button>
+                <van-button v-else-if="prize.receiving.status==3" class="btn outbtn" size="small">确认取出</van-button>
+                <van-button v-else class="btn gobtn" size="small" @click="showAlert(prize.id)">去发货</van-button>
+            </div>
+            自提物品
+            <div v-else class="footer" slot="footer">
+                <van-button v-if="prize.receiving.status==1" class="btn gobtn" size="small" @click="showPopup(prize.id)">未取货</van-button>
+                <van-button v-else class="btn outbtn" size="small">已取货</van-button>
+            </div>
+        </van-panel> -->
+      </van-list>
+        <van-dialog title= '嘀嗒比赛' v-model="showDialog" show-cancel-button :before-close="beforeClose">
+            <van-field v-model="express_name" label="快递公司" placeholder="请输入快递公司名称"/>
+            <van-field v-model="express_number" label="快递单号" placeholder="请输入快递单号"/>
+        </van-dialog>
+        <number-word-input ref="number_word_input" @goodsIdAjax="goodsIdAjax($event)"></number-word-input>
     </div>
 </template>
 
 <script>
 import img from "../../../assets/logo.png";
 import numberWordInput from "../../../components/numberWordInput.vue";
-import didaList from "../../../components/didaList.vue";
 import { timestamp_switch_time } from "lputils";
 export default {
   data() {
     return {
       prizeList: [], //兑奖信息列表
+      loading: false,
+      finished: false,
       imageURL: img,
       showDialog: false,
       express_name: "",
       express_number: "",
+      pagesize: 10, //每页显示条数
+      currentPage: 1, //当前页(默认从第一页开始)
+      total: null, //总记录数
       prizesId: null //选中的奖品id
     };
   },
   components: {
-    numberWordInput,
-    didaList
+    numberWordInput
   },
   methods: {
     //获取兑奖信息列表
-    getPrizeList(data) {
-      this.total = data.total;
-      this.prizeList = data.list;
-      //数据处理(确保数据存在)
-      if (this.prizeList.length < 0) {
-        this.prizeList.forEach(p => {
-          this.prizeList.time = timestamp_switch_time(p.prize.time);
-          this.prizeList.matchdec = `${p.match.name}第${p.match.ranking}名`;
-          if (p.prize.type == 0) this.prizeList.prizeType = "邮寄奖品";
-          else this.prizeList.prizeType = "自取奖品";
-          this.receivingDec = `收货地址${p.receiving.address} ${
-            p.receiving.consignee
-          } ${p.receiving.mobile}`;
+    getPrizeList(pagesize, currentpage) {
+      this.http.prizes
+        .prizeList({
+          pagesize: pagesize,
+          currentpage: currentpage
+        })
+        .then(res => {
+          this.total = res.data.total;
+          this.prizeList = res.data.prizeList;
+          //数据处理
+          this.prizeList.forEach(p => {
+            this.prizeList.time = timestamp_switch_time(p.prize.time);
+            this.prizeList.matchdec = `${p.match.name}第${p.match.ranking}名`;
+            if (p.prize.type == 0) this.prizeList.prizeType = "邮寄奖品";
+            else this.prizeList.prizeType = "自取奖品";
+            this.receivingDec = `收货地址${p.receiving.address} ${
+              p.receiving.consignee
+            } ${p.receiving.mobile}`;
+          });
+          //加载数据成功后设置loading隐藏
+          this.loading = false;
+        })
+        .catch(() => {
+          this.total = 5;
+          //请求出错的时候隐藏加载动画
+          for (let i = 0; i < this.pagesize; i++) {
+            this.prizeList.push(i);
+            this.loading = false;
+          }
         });
-      }
     },
     //无限加载数据
+    onLoad() {
+      setTimeout(() => {
+        this.getPrizeList(this.pagesize, this.currentPage);
+        if (this.currentPage >= this.total && this.total != null)
+          this.finished = true;
+        else this.currentPage++;
+      }, 500);
+    },
     goodsIdAjax(value) {
       console.log(value);
       console.log(this.prizesId);
