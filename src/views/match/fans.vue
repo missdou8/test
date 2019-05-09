c7591da98f6352f39e6380dc1a0dc285cfe0ecb8
yuhe <yuhe@happypoker.cn>
2019-05-09 15:27:55 +0800

    新增感言页

---
+++ src/views/match/fans.vue
@@ -0,0 +1,111 @@
+<template>
+    <div id="fans_box">
+        <ul class="fans_list">
+            <li class="fans_item" v-for="(item , index) in fansList">
+                <div class="fans_img">
+                    <img :src="item.img" alt="">
+                </div>
+                <div class="fans_name">
+                    <span>{{item.fans_name}}</span>
+                </div>
+                <div class="fans_time">
+                    <span>{{item.fans_time}}</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fans",
+        data(){
+            return {
+                fansList:[
+                    {
+                        img:'',
+                        fans_name:'奥特曼',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'小怪兽',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'潘多拉',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'奥德赛',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'詹姆斯',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'杜兰特',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'科比·布莱恩特',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'乔丹',
+                        fans_time:'2019-03-04 09:00:49'
+                    },
+                    {
+                        img:'',
+                        fans_name:'疾风剑豪',
+                        fans_time:'2019-03-04 09:00:49'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #fans_box{
+        background-color: #ffffff;
+    }
+    .fans_list{
+        display: flex;
+        flex-direction: column;
+    }
+    .fans_item{
+        margin-left: .3rem;
+        border-bottom: .01rem solid #c0c4cc;
+        display: flex;
+        height: 1rem;
+        align-items: center;
+    }
+    .fans_item > div{
+        align-items: center;
+    }
+    .fans_img{
+        padding-right: .15rem;
+    }
+    .fans_img > img{
+        display: block;
+        width: .8rem;
+        height: .8rem;
+        border-radius: .8rem;
+        background-color: #c0c4cc;
+    }
+    .fans_time{
+        color: #c0c4cc;
+        font-size: .22rem;
+        flex: 1;
+        text-align: right;
+        padding-right: .3rem;
+    }
+</style>
---
+++ src/views/match/recollections.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <DidaCommentList
+                class="comments_list"
+                v-for="(item,index) in commentsList"
+                :key="`comment${index}`"
+                :data="item"
+                :type="0"
+                @next="commentClick"
+                :disabeld="true"
+                :isComment="true"
+                @toDetail="commentToDetail"
+                @getImgSrc="bigger"
+        ></DidaCommentList>
+    </div>
+
+</template>
+
+<script>
+    import icon from "../../assets/icon.png";
+    import cover from "../../assets/banner_task.png";
+    import { ImagePreview } from "vant";
+    export default {
+        name: "recollections",
+        data(){
+            return {
+                commentsList: [], //评论列表信息
+                isNewComment: false,
+            }
+        },
+        methods:{
+            bigger(src) {
+                ImagePreview({
+                    images: [src],
+                    onClose() {
+                        // do something
+                    }
+                });
+            },
+            commentToDetail(data) {
+                this.toDetail(JSON.parse(data).matchId);
+            },
+            commentClick(data) {
+                this.$router.push({
+                    path: "/match/comment",
+                    query: {
+                        data: JSON.stringify(data)
+                    }
+                });
+            },
+        },
+        created() {
+            return this.http.prizes
+                .commentsList({ pagesize: this.pageSize, currentpage: this.matchPage })
+                .then(res => {
+                    let data = res.data;
+                    this.commentsList = this.commentsList.concat(data.commentsList);
+                    console.log(data)
+                    return data;
+                });
+        }
+    }
+</script>
+
+<style scoped>
+    .comments_list {
+        margin-top: 0.13rem;
+        line-height: 1;
+    }
+</style>
