703433fa6483687bac78ca12aff93c96fe309609
Zyan <zhaoyan@happypoker.cn>
2019-05-09 15:03:23 +0800

    声明公共颜色

---
+++ src/components/button/LargeButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn">{{slot}}</button>
+</template>
+
+<style scoped>
+.btn {
+  background-color: var(--color-yellow);
+  color: var(--font-color-black);
+  font-size: 0.32rem;
+  width: 7.2rem;
+  height: 0.95rem;
+}
+</style>
+
